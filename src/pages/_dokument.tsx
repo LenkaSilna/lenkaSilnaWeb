a81d6a3c20c441bd9f43c7dbd57035492ca1165f
Lenka Silná <lenka.baricova@post.cz>
2024-09-18 12:38:41 +0200

    favicon

--- src/pages/_dokument.tsx
+++ src/pages/_dokument.tsx
@@ -32,10 +32,10 @@ class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
-          <link rel="icon" href="/images/vite.svg" />
+          <link rel="icon" href="/images/vite.svg" type="image/svg+xml" />
+          <link rel="shortcut icon" href="/images/vite.ico" />
           <meta name="viewport" content="width=device-width, initial-scale=1.0" />
           <meta name="theme-color" content="#242424" />
-
         </Head>
         <body>
           <Main />
